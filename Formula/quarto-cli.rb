5faed7213af54b81d59ae16fd4b046ee2ddbfb07
Jassiel O. <a2021117600@estudiantes.upsa.edu.bo>
2025-08-03 18:00:02 -0400

    back to version

--- Formula/quarto-cli.rb
+++ Formula/quarto-cli.rb
@@ -3,6 +3,7 @@
 class QuartoCli < Formula
   desc "Scientific and technical publishing system built on Pandoc"
   homepage "https://www.quarto.org/"
+  version "1.7.32"
   license "MIT"
 
   # --------------------------
