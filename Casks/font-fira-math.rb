8cdada060107f207c5bc846385dd2364074f781a
Jassiel O. <a2021117600@estudiantes.upsa.edu.bo>
2025-08-03 17:43:57 -0400

    non verified

--- Casks/font-fira-math.rb
+++ Casks/font-fira-math.rb
@@ -2,8 +2,7 @@ cask "font-fira-math" do
   version "0.3.4"
   sha256 "2028cbd3dd4d8c0cf1608520eb4759956a83a67931d7b6d8e7c313520186e35b"
 
-  url "https://github.com/firamath/firamath/releases/download/v#{version}/FiraMath-Regular.otf",
-      verified: "github.com/firamath/firamath/"
+  url "https://github.com/firamath/firamath/releases/download/v#{version}/FiraMath-Regular.otf"
   name "FiraMath"
   desc "Sans-serif font with Unicode math support"
   homepage "https://github.com/firamath/firamath"
